c1b7b6728b8412b5348b0c0dcc2fb174e109b78b
leohgomez <corretor.leogomes@gmail.com>
2026-03-06 11:09:44 -0300

    vercel

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://qazhveotdnktxhcdahwt.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFhemh2ZW90ZG5rdHhoY2RhaHd0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI3ODA4NTYsImV4cCI6MjA4ODM1Njg1Nn0.zCtUOodXrPej49m3Wa2kWKWj9NDjsJ94DrlUrv1bCOw';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
